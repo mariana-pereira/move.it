5ddd6a2f87498733ee1e07f3ce682da09387d5c5
Mariana <marianadealmeidapereira@outlook.com>
2021-02-25 09:14:05 -0300

    fourth day finished

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 const Profile: React.FC = () => {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/26336279?s=460&u=64439e413ef11e1c1d5ab507a37f6aa94e59546e&v=4" alt="Mariana Pereira"/>
@@ -8,7 +12,7 @@ const Profile: React.FC = () => {
         <strong>Mariana Pereira</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
